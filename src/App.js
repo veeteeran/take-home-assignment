4990ae5331e30b56dcfd4e522acc428bc52919f6
Viet <vietnamtran@gmail.com>
2021-03-10 14:52:07 -0600

    Add first go at solution, using for of loop not necessary

--- src/App.js
+++ src/App.js
@@ -19,6 +19,29 @@ This      is a second paragraph with extraneous whitespace.`);
 
   const transformText = input => {
     let output = input;
+    let word = "";
+    // Stripping all spaces won't work in case of valid newline
+    // let newStr = output.replace(/\s+/gm, " ")
+    let outputArr = output.split(" ")
+    let newOutput = ""
+    // Use a regular for loop after refactor to make use of index
+    for (word of outputArr) {
+      // Keep the two new lines, assumuming two newlines are always valid
+      if (word.indexOf('\n\n') !== -1) {
+        console.log("contains two newlines")
+      } else if (word.indexOf('\n') !== -1) { // Replace the newline with a space
+        // get index of array element
+        const index = outputArr.indexOf(word)
+        console.log("contains newline")
+        const newWord = word.replace(/\n/, " ")
+        // console.log(newWord)
+        // console.log(index)
+        outputArr.splice(index, 1, newWord)
+        // newOutput = newWord.join(" ")
+      }
+      // newOutput = word.join(" ")
+    }
+    console.log(outputArr);
     // Max length of lines is 80 characters
     // If 80th character is in a word, break at previous space
     // A word more than 80 characters can stay on one line
